d15d7edaf1d493cad6701a03a99d5b69e8f36065
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2025-06-09 22:58:43 +0700

    Responsive Details

---
+++ index.js
@@ -0,0 +1,16 @@
+  $(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+  });
+
+  var typed = new Typed("#typed", {
+    strings: [
+      "University Student",
+      "Future",
+      "Tech Expert",
+      "Ambassador",
+      "Star",
+    ],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+  });
